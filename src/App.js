23dafd4cde45239e0a7fe4a90322de8235f289a0
Guhan <you@example.com>
2022-04-05 16:02:04 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import Sidebar from './SideBar.js';
+import Topbar from './Topbar.js';
+import Dashboard from './Dashboard';
+import Student from './Student';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import StudentView from './StudentView';
+import StudentEdit from './StudentEdit';
+import { UserProvider } from './UserContext';
+import { useState } from 'react';
+import CreateStudent from './CreateStudent';
+import Teacher from './Teacher';
+import CreateTeacher from './CreateTeacher';
+import TeacherView from './TeacherView';
+import TeacherEdit from './TeacherEdit';
 
 function App() {
+  const [users,setUsers]=useState([]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div id="wrapper">
+        <UserProvider value={{users,setUsers}}>
+        <Sidebar />
+        <div id="content-wrapper" class="d-flex flex-column">
+          <div id="content">
+            <Topbar />
+            <div class="container-fluid">
+            <Routes>
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/student" element={<Student />} />
+                <Route path="/student-create" element={<CreateStudent/>}/>
+                <Route path="/student-view/:id" element={<StudentView/>}/>
+                <Route path="/student-edit/:id" element={<StudentEdit/>}/>
+                <Route path="/teacher" element={<Teacher />} />
+                <Route path="/teacher-create" element={<CreateTeacher/>}/>
+                <Route path="/teacher-view/:id" element={<TeacherView/>}/>
+                <Route path="/teacher-edit/:id" element={<TeacherEdit/>}/>
+              </Routes>
+            </div>
+          </div>
+        </div>
+        </UserProvider>
+      </div>
+    </BrowserRouter>
+
   );
 }
 
